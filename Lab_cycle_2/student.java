f427c8214638ced0f8e97d2dfff692164774dea6
TinuBose <117371766+TinuBose@users.noreply.github.com>
2023-06-12 06:44:15 +0530

    Update student.java

--- Lab_cycle_2/student.java
+++ Lab_cycle_2/student.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
 
-class mark {
+class Mark {
     int m;
-    int a[];
+    int[] a;
     int sum = 0;
     float per = 0;
     Scanner s = new Scanner(System.in);
 
-    mark() {
-        System.out.print("enter the number of subjects : ");
+    Mark() {
+        System.out.print("Enter the number of subjects: ");
         m = s.nextInt();
         a = new int[m];
     }
 
     void operation() {
-        System.out.println("\nenter mark of " + m + " subjects out of 100 :");
+        System.out.println("\nEnter marks of " + m + " subjects out of 100:");
         for (int i = 0; i < m; i++) {
             a[i] = s.nextInt();
+            if (a[i] > 100 || a[i] < 0) {
+                System.out.print("Enter correct mark for subject " + (i + 1) + ": ");
+                a[i] = s.nextInt();
+            }
             sum = sum + a[i];
         }
-        System.out.print("\ntotal marks obtained : " + sum);
-        per = (sum / m * 100) / 100;
-        System.out.print("\npercentage : " + per + "%");
-    }
 
+        System.out.println("\nTotal marks obtained: " + sum);
+        per = (float) sum / (m * 100) * 100;
+        System.out.println("Percentage: " + per + "%");
+    }
 }
 
 public class student {
     public static void main(String args[]) {
-        mark m = new mark();
+        Mark m = new Mark();
         m.operation();
     }
-
 }
