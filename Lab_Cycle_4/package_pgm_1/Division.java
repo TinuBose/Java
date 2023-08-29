6833c2806f0bce0762384b1c16fe492d6d34af74
TinuBose <117371766+TinuBose@users.noreply.github.com>
2023-08-29 20:30:32 +0530

    Add files via upload

---
+++ Lab_Cycle_4/package_pgm_1/Division.java
@@ -0,0 +1,8 @@
+package Arithmetic;
+import java.util.*;
+interface Div{
+ void division(float a, float b);}
+public class Division implements Div {
+ public void division(float a, float b){
+ System.out.println("The quotient of two numbers is:
+"+(a/b));}}
---
+++ Lab_Cycle_4/package_pgm_1/Subtraction.java
@@ -0,0 +1,8 @@
+package Arithmetic;
+import java.util.*;
+interface Sub{void subtraction(float a, float b);}
+32
+public class Subtraction implements Sub{
+ public void subtraction(float a, float b){
+ System.out.println("The difference of two numbers is:
+"+(a-b));}}
